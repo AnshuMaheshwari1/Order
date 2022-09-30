251b750d503af0cb66652e516806bbfc7e79c5da
AnshuMaheshwari1 <89083600+AnshuMaheshwari1@users.noreply.github.com>
2022-10-01 02:17:46 +0530

    Adding standalone folder

---
+++ order-standalone/src/main/java/com/anshu/order/OrderParser.java
@@ -0,0 +1,108 @@
+package com.anshu.order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class OrderParser {
+	
+//	private static String basepath = "E:\\eclipse-workspace\\order\\resources\\";
+//	private static String csvSplitBy = ",";
+//	private static int numOfCols = 4; 
+//	private static String outPutFileName = "output.json";
+
+	public static void main(String[] args) {
+
+		// String fileName = args[0];
+		List<RunnableTask> tasks = new ArrayList<>();
+		List<Thread> threads = new ArrayList<>();
+		
+		for (String file : args) {
+//			String[] input = file.split("\\.");
+//			String fileType = input[1];
+			RunnableTask task = new RunnableTask(file);
+			Thread t = new Thread(task);
+			t.start();
+			tasks.add(task);
+			threads.add(t);
+			
+			for (Thread t1 : threads) {
+				try {
+					t1.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+//			if ("csv".equals(fileType)) {
+//				saveObjectsInJson(csvParser(file));
+//			} else if ("json".equals(fileType)) {
+//				saveObjectsInJson(jsonFileToObjects(file));
+//			}
+		}
+	}
+
+//	private static List<Order> csvParser(String fileName) {
+//		int lineNumber = -1;
+//		BufferedReader br;
+//		List<Order> orderList = new ArrayList<>();
+//		try {
+//			br = new BufferedReader(new FileReader(basepath + fileName));
+//
+//			String line = "";
+//			
+//			while ((line = br.readLine()) != null) {
+//
+//				String[] cols = line.split(csvSplitBy);
+//				// order.list
+//				lineNumber++;
+//				if (cols.length == numOfCols && lineNumber != 0) {
+//					Order order = new Order();
+//					order.setOrderId(cols[0]);
+//					order.setAmount(Float.parseFloat(cols[1]));
+//					order.setCurrency(cols[2]);
+//					order.setComment(cols[3]);
+//					order.setFileName(fileName);
+//					order.setLineNumber(lineNumber);
+//					orderList.add(order);
+//				}
+//			}
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return orderList;
+//	}
+	
+//	private static void saveObjectsInJson(List<Order> orderList) {
+//		ObjectMapper mapper = new ObjectMapper();
+//		try {
+//			for (Order order : orderList) {
+//				mapper.writeValue(new FileWriter(basepath + outPutFileName, true), order);
+//			}
+//			
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}		
+//	}
+//	
+//	private static List<Order> jsonFileToObjects(String fileName) {
+//		ObjectMapper mapper = new ObjectMapper();
+//		List<Order> orderList = new ArrayList<Order>();
+//		try {
+//			orderList = mapper.readValue(new File(basepath + fileName), new TypeReference<List<Order>>() {});
+//			for (int i = 0; i < orderList.size(); i++) {
+//				orderList.get(i).setLineNumber(i+1);
+//				orderList.get(i).setFileName(fileName);
+//			}
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return orderList;
+//	}
+
+
+}
+
